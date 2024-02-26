00df333771b55cb9cd1aac43e4410b723d5a5916
avner <avner.gideoni@gmail.com>
2024-02-26 21:32:17 +0200

    compile wip

--- src/compile.c
+++ src/compile.c
@@ -5,13 +5,14 @@
 #include "compile.h"
 #include "global_constants.h"
 
-int preCompile(const char *arg)
+int compile(const char *arg)
 {
 
     /* step 1*/
     int data_count = 0, instruction_count = 0;
-             FILE *source, *destination;
-             char buffer [MAX_LINE_LENGTH];
+    FILE *source, *destination;
+    char *buffer=NULL; command[MAX_PARAM_SIZE], first_param[MAX_PARAM_SIZE]
+    int result;
 
     char *fileName = (char *)calloc(strlen(arg) + 4, sizeof(char));
 
@@ -29,16 +30,17 @@ int preCompile(const char *arg)
         free(fileName);
         exit(1);
     }
-    /* step 2 - read line */
 
-    while (read_line(source, **buffer))
-    { 
-        char buffer[MAX_LINE_LENGTH];
-        read_line(source, **buffer);
-        printf(buffer);
-    }
+    compile_first_stage();
 
-    strcat(fileName, ".ob");
+/*step 16- if errors stop*/
+/*step 17- update data with value IC+100 in symbol table*/
+
+    compile_second_step();
+
+
+exit(0);
+        strcat(fileName, ".ob");
     if ((destination = fopen(fileName, "rw")) == NULL)
     {
         fprintf(stderr, "Error! Failed open file %s\n", fileName);
@@ -47,4 +49,39 @@ int preCompile(const char *arg)
     }
 
     return 0;
+}
+
+
+
+int compileFirstStage(){
+
+    /* step 2 - read line */
+
+    while (read_line(source, &buffer))
+    {
+        printf("%s\n",buffer);
+        result = parse_command(buffer, command, first_param)
+    }
+
+/* step 3 - if type== define*/
+/* step 4 - put define in mdefine table.*/
+/* step 5+6  - is sybmol ? */
+/* step 7 - is data or string */
+/* step 8 - put symbol in symbol table */
+/* step 9 - identify data/params and put them in mem table (which?) update DC */
+/* step 10 - if extern or entry */
+/* step 11 - if extern put in etx table*/
+/* step 12 - if symbol put in symbol table*/
+/* step 13 - lookup operation in table*/
+/* step 14 - calculate L , build binary code of first word*/
+/* step 15 - IC = IC + L . goto #2*/
+
+return 0;
+}
+
+
+
+int    compile_second_step(){
+
+
 }
--- src/compile.h
+++ src/compile.h
@@ -1,4 +1,4 @@
 #ifndef __COMPILE__
 #define __COMPILE__
-
+int compile(const char *arg);
 #endif
