fde40f7840b87032c770685fb142f57ce203275a
avner <avner.gideoni@gmail.com>
2024-03-02 15:51:12 +0200

    LanguageUtils

--- src/compileFirstStage.c
+++ src/compileFirstStage.c
@@ -10,72 +10,7 @@
 #include "datamodel.h"
 #include "memoryUtils.h"
 #include "language.h"
-
-int legalLabel(char *label_name, Symbol **symbols, size_t symbol_count) /*not good enough*/
-{
-    As_Command assember_commands[NUM_OF_COMMANDS_IN_LANGUAGE] = AS_COMMAND_LIST;
-    const char registers[NUNMER_OF_REGISTERS][3] = RESIGTERS;
-
-    int i;
-    if (isalpha(label_name[0]) == 0)
-    {
-        fprintf(stderr, "The first character of the label isn't a letter\n");
-        return FIRST_LETTER_IS_NOT_A_LETTER;
-    }
-    for (i = 0; i < NUM_OF_COMMANDS_IN_LANGUAGE; i++)
-    {
-        if (strcmp(label_name, (assember_commands[i].command_name)) == 0)
-        {
-            fprintf(stderr, "The name of the label is already taken\n");
-            return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-        }
-    }
-
-    for (i = 0; i < NUNMER_OF_REGISTERS; i++)
-    {
-        if (strcmp(label_name, (registers[i])) == 0)
-        {
-            fprintf(stderr, "The name of the label is already taken\n");
-
-            return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-        }
-    }
-    for (i = 0; i < symbol_count; i++)
-    {
-        if (strcmp(label_name, (symbols[i]->name)) == 0)
-        {
-            fprintf(stderr, "The name of the label is already taken\n");
-            return ERR_LABEL_OR_NAME_IS_TAKEN;
-        }
-    }
-    /*for (i = 0; i < macro_amount;i++){
-        if(strcmp(label_name, (macro_names[i])) != 0){
-            return ERR_LABEL_OR_NAME_IS_TAKEN; ~illegal~}
-    }*/
-
-    if (strcmp(label_name, ".data") == 0)
-    {
-        fprintf(stderr, "Illegal name for label\n");
-        return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-    }
-    if (strcmp(label_name, ".string") == 0)
-    {
-        fprintf(stderr, "Illegal name for label\n");
-        return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-    }
-    if (strcmp(label_name, ".entry") == 0)
-    {
-        fprintf(stderr, "Illegal name for label\n");
-        return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-    }
-    if (strcmp(label_name, ".extern") == 0)
-    {
-        fprintf(stderr, "Illegal name for label\n");
-        return ERR_LABEL_OR_NAME_IS_TAKEN; /*illegal*/
-    }
-
-    return 0;
-}
+#include "languageUtils.h"
 
 int compileFirstStage(const char *filename, Symbol **symbols, size_t *symbol_count, Opcode **opcodes, size_t *opcode_count, Line_params **line_params, size_t *line_params_count)
 {
---
+++ src/languageUtils.h
@@ -0,0 +1,10 @@
+#ifndef __LANGUAGE_UTILS__
+#define __LANGUAGE_UTILS__
+
+#include <stdlib.h>
+#include "language.h"
+#include "datamodel.h"
+
+int legalLabel(char *label_name, Symbol **symbols, size_t symbol_count);
+
+#endif
