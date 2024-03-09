869a90d50380cf3ec0ef7d899f3c383e170ca4ee
avner <avner.gideoni@gmail.com>
2024-03-09 13:42:00 +0200

    memory comments

--- src/Utils/memoryUtils.c
+++ src/Utils/memoryUtils.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "../global_constants.h"
 
+/**
+ * Function to push a new element into an array.
+ *
+ * @param array The array to push the element into.
+ * @param size The size of the array.
+ * @param element_size The size of each element in the array.
+ * @param new_element The new element to be pushed into the array.
+ */
 void push(void **array, size_t *size, size_t element_size, const void *new_element)
 {
     (*size)++;
