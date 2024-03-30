5e42a838cc5407479918ee5a684f7b82cf7eb2bc
avner <avner.gideoni@gmail.com>
2024-03-30 14:27:04 +0300

    tests updated

--- src/compilationStages/updateDataSymbols.c
+++ src/compilationStages/updateDataSymbols.c
@@ -9,10 +9,6 @@ void update_data_address(Data_model *data_model)
 
     int i = 0;
     size_t instruction_count = data_model->instruction_count;
-    printf("%d %d\n", data_model->data_count, data_model->symbol_count);
-    if (instruction_count == 0)
-        return;
-
     for (i = 0; i < data_model->symbol_count; i++)
         if (data_model->symbols[i].type == DATA)
             data_model->symbols[i].value += instruction_count + CODE_START_ADDRESS;
